9f84421846fcc0ae1837cefae9e3d2ed33645caf
carlos triana <carlosalonsotrianas@gmail.com>
2024-05-29 11:42:43 -0500

    Mis cambios antes de la actualización

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 
 @Injectable({
@@ -7,17 +7,7 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class FirebaseService {
 
-  constructor( firestore: AngularFirestore ) { }
-
-  // Ejemplo: obtener una colección
-  getCollection(collectionName: string) {
-    return this.firestore.collection(collectionName).snapshotChanges();
-  }
-
-  // Ejemplo: añadir un documento
-  addDocument(collectionName: string, data: any) {
-    return this.firestore.collection(collectionName).add(data);
-  }
 
 
+  constructor( ) { }
 }
